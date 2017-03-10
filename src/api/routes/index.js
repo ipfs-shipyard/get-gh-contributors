f04abb6f592a7f97e578616d389c73da2fd2c9e6
David Dias <daviddias.p@gmail.com>
2017-03-10 13:21:15 +0000

    add root endpoint

--- src/api/routes/index.js
+++ src/api/routes/index.js
@@ -1,3 +1,10 @@
 module.exports = (server, dataPath) => {
   require('./contributors')(server, dataPath)
+  server.route({
+    method: 'GET',
+    path: '/',
+    config: {
+      handler: (request, reply) => reply('OHAI!')
+    }
+  })
 }
